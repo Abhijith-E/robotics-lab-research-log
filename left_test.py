ca00856b75d2807af71d9043405243a78c253f74
Abhijith-E <20eabhijith@gmail.com>
2026-03-12 14:17:21 +0530

    left movement updated

--- left_test.py
+++ left_test.py
@@ -4,10 +4,11 @@ from motor_driver import MotorDriver
 robot = MotorDriver()
 
 def move_left(speed=20, duration=2):
-    robot.set_motor_speed(1, speed)
-    robot.set_motor_speed(2, -speed)
-    robot.set_motor_speed(3, -speed)
-    robot.set_motor_speed(4, speed)
+    # Strafe LEFT
+    robot.set_motor_speed(1, -speed)   # Front Left
+    robot.set_motor_speed(2, speed)    # Front Right
+    robot.set_motor_speed(3, speed)    # Rear Left
+    robot.set_motor_speed(4, -speed)   # Rear Right
 
     time.sleep(duration)
     stop()
