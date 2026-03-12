d6d9ce80e2ad69afa3427aee8f07ba9d1390eef2
Abhijith-E <20eabhijith@gmail.com>
2026-03-12 14:43:59 +0530

    diagonal movement updated and corrected

--- d_backward_right_test.py
+++ d_backward_right_test.py
@@ -3,11 +3,11 @@ from motor_driver import MotorDriver
 
 robot = MotorDriver()
 
-def move_back_diagonal_right(speed=20, duration=2):
-    robot.set_motor_speed(1, -speed)
+def move_front_diagonal_right(speed=20, duration=2):
+    robot.set_motor_speed(1, speed)
     robot.set_motor_speed(2, 0)
     robot.set_motor_speed(3, 0)
-    robot.set_motor_speed(4, -speed)
+    robot.set_motor_speed(4, speed)
 
     time.sleep(duration)
     stop()
@@ -17,6 +17,6 @@ def stop():
         robot.set_motor_speed(m,0)
 
 try:
-    move_back_diagonal_right()
+    move_front_diagonal_right()
 finally:
     stop()
--- d_forward_right_test.py
+++ d_forward_right_test.py
@@ -3,11 +3,11 @@ from motor_driver import MotorDriver
 
 robot = MotorDriver()
 
-def move_front_diagonal_right(speed=20, duration=2):
-    robot.set_motor_speed(1, speed)
-    robot.set_motor_speed(2, 0)
-    robot.set_motor_speed(3, 0)
-    robot.set_motor_speed(4, speed)
+def move_back_diagonal_left(speed=20, duration=2):
+    robot.set_motor_speed(1, 0)
+    robot.set_motor_speed(2, -speed)
+    robot.set_motor_speed(3, -speed)
+    robot.set_motor_speed(4, 0)
 
     time.sleep(duration)
     stop()
@@ -17,6 +17,6 @@ def stop():
         robot.set_motor_speed(m,0)
 
 try:
-    move_front_diagonal_right()
+    move_back_diagonal_left()
 finally:
     stop()
